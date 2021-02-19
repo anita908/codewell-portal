d56d59f5b85e2deab987913d969db7453d49e019
sunnydtakoyaki <sunny.yang@databased.com>
2021-02-19 16:11:50 -0700

    Work on user entity

--- portal/src/Entities/TestClasses/User.test.ts
+++ portal/src/Entities/TestClasses/User.test.ts
@@ -1,8 +1,23 @@
 import User from '../User'
 
 describe('Test user entity', () => {
-  it('Should have a name', () => {
-    const user = new User('Sunny Yang')
-  expect(user.name).toEqual('Sunny Yang')
+  it('Should have basic user data', () => {
+    const userData = {
+      name: 'Sunny Yang',
+      age: 22,
+      email: 'asdf@mail.com', 
+      phone: 2342342342,
+      username: 'hahasunny',
+      isActive: false,
+      isAdmin: true
+    }
+    const user = new User({ name: userData.name, age: userData.age, email: userData.email, phone: userData.phone, username: userData.username, isActive: userData.isActive, isAdmin: userData.isAdmin })
+  expect(user.name).toEqual(userData.name)
+  expect(user.age).toBe(userData.age)
+  expect(user.email).toEqual(userData.email)
+  expect(user.phone).toEqual(userData.phone)
+  expect(userData.username).toEqual(userData.username)
+  expect(userData.isActive).toBe(false)
+  expect(userData.isAdmin).toBe(true)
   })
 })
--- portal/src/Entities/User.ts
+++ portal/src/Entities/User.ts
@@ -1,12 +1,78 @@
 class User {
+  private _age: number
+  private _email: string
+  private _isActive: boolean
+  private _isAdmin: boolean
   private _name: string
+  private _phone: number
+  private _username: string
   
-  constructor(name: string) {
+  constructor(params: {name: string, email: string, phone: number, age: number, username: string, isActive: boolean, isAdmin: boolean}) {
+    const { age, email, name, phone, username, isActive, isAdmin } = params
+    this._age = age
+    this._email = email
     this._name = name
+    this._phone = phone
+    this._username = username
+    this._isActive = isActive
+    this._isAdmin = isAdmin
+  }  
+
+  
+  public set isAdmin(isAdmin: boolean) {
+    this._isAdmin = isAdmin
+  }
+  
+  public get isAdmin() : boolean {
+    return this._isAdmin
+  }
+  
+  public set isActive(isActive: boolean) {
+    this._isActive = isActive
+  }
+  
+  public set username(username : string) {
+    this._username = username
+  }
+
+  public set age(age: number) {
+    this._age = age
+  }
+
+  public set email(email: string) {
+    this._email = email
+  }
+
+  public set name(name: string) {
+    this._name = name
+  }
+
+  public set phone(phone: number) {
+    this._phone = phone
+  }
+
+  public get isActive(): boolean {
+    return this._isActive
+  }
+
+  public get username() : string {
+    return this._username
+  }
+
+  public get age(): number {
+    return this._age
+  }
+
+  public get email(): string {
+    return this._email
   }
 
   public get name(): string {
     return this._name
+  }
+
+  public get phone(): number {
+    return this._phone
   }
 }
 
