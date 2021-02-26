3f994cc8c5cbdbb0476c11af21ad7916a5028473
sunnydtakoyaki <sunny.yang@databased.com>
2021-02-25 20:17:41 -0700

    Add and implement react router dom Fix jsx flag issue Add login and home view

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from './Routes/Home/Home'
+import Login from './Routes/Login/Login'
 import './App.css'
 
-function App() {
+function App(): ReactElement {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path='/' component={Home} />
+      <Route path='/login' component={Login} />
+    </Switch>
   )
 }
 
