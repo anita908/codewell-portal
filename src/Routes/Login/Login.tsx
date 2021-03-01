3675a5094a876d337bc4c7616c2e090108c8fccf
sunnydtakoyaki <sunny.yang@databased.com>
2021-02-28 22:24:39 -0700

    Add email and password input boxes

--- src/Routes/Login/Login.tsx
+++ src/Routes/Login/Login.tsx
@@ -5,7 +5,18 @@ function Login() {
   return (
     <div id='login'>
       <div className='login-illustration' />
-      <div className='login-input' />
+      <div className='login-input'>
+        <div className='login-inputGroup'>
+          <div className='login-email'>
+            <label htmlFor='email'>Email: </label>
+            <input type='email' id='email' name='email' />
+          </div>
+          <div className='login-password'>
+            <label htmlFor='password'>Password: </label>
+            <input type='password' id='password' name='password' />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
