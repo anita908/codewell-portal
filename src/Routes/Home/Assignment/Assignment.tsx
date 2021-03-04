fee45f42c153b3abd882331dc7928c450a8e9904
sunnydtakoyaki <sunny.yang@databased.com>
2021-03-03 23:05:33 -0700

    Add lesson component Remove Main Fix component names

--- src/Routes/Home/Assignment/Assignment.tsx
+++ src/Routes/Home/Assignment/Assignment.tsx
@@ -1,17 +1,21 @@
 import React, { Component, ReactElement } from 'react'
 import './style.css'
 
-class Home extends Component {
+class Assignment extends Component {
   render(): ReactElement {
     return (
       <div id='assignment'>
         <div className='assignment-header'>
           <h3>Assignment</h3>
         </div>
-        <div className='assignment-content'>Assignments</div>
+        <div className='assignment-content'>
+          <div className='assignment-lessonCard'>Lesson 7: Function</div>
+          <div className='assignment-lessonCard'>Lesson 7: Function</div>
+          <div className='assignment-lessonCard'>Lesson 7: Function</div>
+        </div>
       </div>
     )
   }
 }
 
-export default Home
+export default Assignment
