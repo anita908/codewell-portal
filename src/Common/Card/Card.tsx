e02e1c5180cdfdc37f7cc177f6294048ed62282a
sunnydtakoyaki <sunny.yang@databased.com>
2021-03-04 15:22:16 -0700

    Modify style

--- src/Common/Card/Card.tsx
+++ src/Common/Card/Card.tsx
@@ -2,15 +2,20 @@ import React, { Component, ReactElement } from 'react'
 import './style.css'
 
 type Props = {
+  content?: any
   header: string
+  title?: string
 }
 
 class Card extends Component<Props, {}> {
   render(): ReactElement {
-    const { header } = this.props
+    const { content, title, header } = this.props
+
     return (
       <div className='card'>
-        <p>{header}</p>
+        <h3 className='card-header'>{header}</h3>
+        <p>{title}</p>
+        <p>{content}</p>
       </div>
     )
   }
