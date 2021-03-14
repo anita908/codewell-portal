5fa0e4ac0b6cc6ec9f1c996ef3f91a2fe6b8c550
sunnydtakoyaki <sunny.yang@databased.com>
2021-03-14 16:31:50 -0600

    Able to toggle to show more lessons and assignemnts

--- src/Routes/Home/Assignment/Assignment.tsx
+++ src/Routes/Home/Assignment/Assignment.tsx
@@ -7,20 +7,30 @@ type Prop = {
   lessons: ILesson[]
 }
 
-class Assignment extends Component<Prop, {}> {
+type State = {
+  showMore: boolean
+}
+
+class Assignment extends Component<Prop, State> {
+  state = {
+    showMore: false
+  }
+
   render(): ReactElement {
     const { lessons } = this.props
+    const { showMore } = this.state
+    const displayLessons: ILesson[] = showMore ? lessons : [lessons[0], lessons[1]] // cur & next
 
     return (
       <div id='assignment'>
         <div className='assignment-header'>
           <h3>Assignments</h3>
-          <details className='assignment-toggleExpandButton'>
-            <summary>Expand</summary>
+          <details onClick={this.toggleSection} className='assignment-toggleExpandButton'>
+            <summary>View More</summary>
           </details>
         </div>
         <div className='assignment-content'>
-          {lessons.map((lesson: ILesson) => {
+          {displayLessons.map((lesson: ILesson) => {
             return (
               <Fragment key={lesson.chapterId}>
                 <Card
@@ -33,6 +43,31 @@ class Assignment extends Component<Prop, {}> {
           })}
         </div>
       </div>
+    )
+  }
+
+  toggleSection = (): void => {
+    this.setState(
+      (prevState) => ({
+        showMore: !prevState.showMore
+      }),
+      () => {
+        const assignmentContent = document.querySelector('.assignment-content') as HTMLElement
+        const assignmentCards = document.querySelectorAll('.assignment-content > .card')
+
+        if (assignmentContent) {
+          assignmentContent.style.gridTemplateColumns = this.state.showMore
+            ? 'repeat(4, 1fr)'
+            : 'repeat(2, 1fr)'
+        }
+
+        if (assignmentCards) {
+          Array.from(assignmentCards).forEach((assignmentCard: HTMLElement) => {
+            // eslint-disable-next-line no-param-reassign
+            assignmentCard.style.width = this.state.showMore ? 'var(--card-width)' : 'auto'
+          })
+        }
+      }
     )
   }
 }
