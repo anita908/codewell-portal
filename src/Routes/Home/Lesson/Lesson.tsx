874f6791611e065abec38389a89572af482dae9e
sunnydtakoyaki <sunny.yang@databased.com>
2021-03-14 16:31:50 -0600

    Able to toggle to show more lessons and assignemnts

--- src/Routes/Home/Lesson/Lesson.tsx
+++ src/Routes/Home/Lesson/Lesson.tsx
@@ -7,17 +7,30 @@ type Prop = {
   lessons: ILesson[]
 }
 
-class Lesson extends Component<Prop, {}> {
+type State = {
+  showMore: boolean
+}
+
+class Lesson extends Component<Prop, State> {
+  state = {
+    showMore: false
+  }
+
   render(): ReactElement {
     const { lessons } = this.props
+    const { showMore } = this.state
+    const displayLessons: ILesson[] = showMore ? lessons : [lessons[0], lessons[1]] // cur & next
 
     return (
       <div id='lesson'>
         <div className='lesson-header'>
           <h3>Lessons</h3>
+          <details onClick={this.toggleSection} className='lesson-toggleExpandButton'>
+            <summary>View More</summary>
+          </details>
         </div>
         <div className='lesson-content'>
-          {lessons.map((lesson: ILesson) => {
+          {displayLessons.map((lesson: ILesson) => {
             return (
               <Fragment key={lesson.chapterId}>
                 <Card
@@ -30,6 +43,30 @@ class Lesson extends Component<Prop, {}> {
           })}
         </div>
       </div>
+    )
+  }
+
+  toggleSection = (): void => {
+    this.setState(
+      (prevState) => ({
+        showMore: !prevState.showMore
+      }),
+      () => {
+        const lessonContent = document.querySelector('.lesson-content') as HTMLElement
+        const lessonCards = document.querySelectorAll('.lesson-content > .card')
+        if (lessonContent) {
+          lessonContent.style.gridTemplateColumns = this.state.showMore
+            ? 'repeat(4, 1fr)'
+            : 'repeat(2, 1fr)'
+        }
+
+        if (lessonCards) {
+          Array.from(lessonCards).forEach((lessonCard: HTMLElement) => {
+            // eslint-disable-next-line no-param-reassign
+            lessonCard.style.width = this.state.showMore ? 'var(--card-width)' : 'auto'
+          })
+        }
+      }
     )
   }
 }
