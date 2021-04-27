9e4d7cf46792ecbb7b8e446ccfd237cd7da6f795
anita908 <anita248846@gmail.com>
2021-04-27 17:18:15 -0600

    create testing colorVariables, Card, Footer, ToggleInput, Select, SideNav, HamburgerMenu

---
+++ src/Common/Form/Input/ToggleInput.test.tsx
@@ -0,0 +1,49 @@
+import { BrowserRouter } from 'react-router-dom'
+import { render } from '@testing-library/react'
+import React from 'react'
+import ToggleInput from './ToggleInput'
+
+let Props = {
+  active: true,
+  type: 'checkbox',
+  value: '',
+  checked: true,
+  onChange: jest.fn()
+}
+
+function getContainer() {
+  return render(
+    <BrowserRouter>
+      <ToggleInput {...Props} />
+    </BrowserRouter>
+  ).container
+}
+
+describe('Test ToggleInput', () => {
+  it('return when type equal checkbox', () => {
+    const container = getContainer()
+    expect(container).toBeDefined()
+  })
+  it('return when type equal is not checkbox and active is true', () => {
+    Props = {
+      active: true,
+      type: '',
+      value: '',
+      checked: true,
+      onChange: jest.fn()
+    }
+    const container = getContainer()
+    expect(container).toBeDefined()
+  })
+  it('return when type equal is not checkbox and active is false', () => {
+    Props = {
+      active: false,
+      type: '',
+      value: 'Test value',
+      checked: true,
+      onChange: jest.fn()
+    }
+    const container = getContainer()
+    expect(container).toBeDefined()
+  })
+})
