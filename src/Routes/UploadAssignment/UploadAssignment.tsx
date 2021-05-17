e6a090adcbb051c5c38c7f2e090bfea13231efb1
anita908 <anita248846@gmail.com>
2021-05-17 17:21:33 -0600

    add previous submissio to uploadAssignment page

--- src/Routes/UploadAssignment/UploadAssignment.tsx
+++ src/Routes/UploadAssignment/UploadAssignment.tsx
@@ -1,8 +1,12 @@
 import React, { ChangeEvent, Component, ReactElement } from 'react'
 import Dropdown from '../../Common/Form/Dropdown/Dropdown'
+import Fetcher from 'Drivers/Fetcher'
 import FileForm from './Form/FileForm'
 import Footer from '../../Common/Footer/Footer'
+import IChapterGradesModel from './Interfaces/IChapterGradesModel'
+import IHomeworkProgress from 'Routes/Home/Interfaces/IHomeworkProgress'
 import SideNav from '../../Common/SideNav/SideNav'
+import UploadAssignmentPresenter from './UploadAssignmentPresenter'
 import UrlForm from './Form/UrlForm'
 import './style.css'
 
@@ -18,20 +22,28 @@ type Props = {
 }
 
 type State = {
+  sessionGradesModel: IChapterGradesModel[]
   uploadFormat: string
 }
+
+const uploadAssignmentPresenter = new UploadAssignmentPresenter(new Fetcher())
 
 const URL = 'Url'
 const FILE = 'File'
 
 class UploadAssignment extends Component<Props, State> {
   state = {
+    sessionGradesModel: [],
     uploadFormat: URL
+  }
+
+  componentDidMount(): void {
+    this.getChapterProgressModels()
   }
 
   render(): ReactElement {
     const { chapterName, homeworkId, lessonNumber, sessionId } = this.props.location.state
-    const { uploadFormat } = this.state
+    const { sessionGradesModel, uploadFormat } = this.state
 
     return (
       <div id='uploadAssignment'>
@@ -42,6 +54,34 @@ class UploadAssignment extends Component<Props, State> {
           <button className='uploadAssignment-back back' onClick={this.back} type='button'>
             Back
           </button>
+          <div>
+            {sessionGradesModel.map((chapterProgress: IChapterGradesModel) => (
+              <div key={chapterProgress.chapterId}>
+                {chapterProgress.chapterNo === lessonNumber ? (
+                  <div>
+                    {chapterProgress.homeworkProgress.map((homework: IHomeworkProgress) => (
+                      <div key={homework.homeworkName}>
+                        {homework.homeworkId === homeworkId ? (
+                          <div>
+                            {homework.submissionUrl ? (
+                              <div>
+                                <a>Your previous submission: </a>
+                                <a target='_blank' rel='noreferrer' href={homework.submissionUrl}>
+                                  Submission
+                                </a>
+                              </div>
+                            ) : (
+                              <p>You haven't submitted any homework yet </p>
+                            )}
+                          </div>
+                        ) : null}
+                      </div>
+                    ))}
+                  </div>
+                ) : null}
+              </div>
+            ))}
+          </div>
           <div className='uploadAssignment-dropdown'>
             <Dropdown onChange={this.selectUploadFormat}>
               <option value={URL}>Url</option>
@@ -66,6 +106,12 @@ class UploadAssignment extends Component<Props, State> {
   selectUploadFormat = (event: ChangeEvent<HTMLSelectElement>): void => {
     const target = event.target as HTMLSelectElement
     this.setState({ uploadFormat: target.value })
+  }
+
+  async getChapterProgressModels(): Promise<void> {
+    this.setState({
+      sessionGradesModel: await uploadAssignmentPresenter.getSessionGradesModel()
+    })
   }
 }
 
